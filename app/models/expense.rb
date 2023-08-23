f7a78eb967dc688622b79afd15d1c35894f53ce8
bottles10 <henryaovare@gmail.com>
2023-08-23 04:44:18 -0400

    Added more validations to expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,9 +2,25 @@ class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :budget, optional: true
 
+  validates :date, presence: true
+  validates :amount, presence: true
+  validates :category, presence: true
+  validates :payment_method, presence: true
+
+  validate :expenses_is_not_future
 
 
   def has_budget?
     budget_id.present?
+  end
+
+  private
+
+  # Expenses date cannot be after tommorrow date
+  def expenses_is_not_future
+    return unless date.present?
+    if self.date > Date.tomorrow
+      errors.add(:date, " Expenses date cannot be future : #{date}")
+    end
   end
 end
