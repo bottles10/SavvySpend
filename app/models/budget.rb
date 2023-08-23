5f32bd8f0304742356ad964bbce4769dd27fb04a
bottles10 <henryaovare@gmail.com>
2023-08-23 04:46:42 -0400

    Added more validations to budget model

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,9 @@
 class Budget < ApplicationRecord
   belongs_to :user
   has_many :expenses, dependent: :destroy
+
+  validates :category, presence: true
+  validates :amount, presence: true
+  validates :time_period, presence: true
+  
 end
