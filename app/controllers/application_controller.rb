ae8a51273543b487bf4b5f9b6eff92d21d50efc7
bottles10 <henryaovare@gmail.com>
2023-08-23 06:19:21 -0400

    Configure redirect to new_income  when login with no income

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
+
+    def after_sign_in_path_for(resource)
+      if resource.is_a?(User) && !resource.income.present?
+        new_income_path
+      else
+        # Default behavior for sign in
+        super
+      end
+    end
   
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :email])
